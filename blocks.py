3f3a601b6802e45d88c32ed4ff8b52130cda8348
GOISQUE-SARRAZIN CORENTIN <corentin.goisque-sarrazin@etu.unistra.fr>
2024-11-21 23:36:14 +0100

    finished python Tetris

--- blocks.py
+++ blocks.py
@@ -79,7 +79,7 @@ class ZBlock(Block):
         super().__init__(id=7)
         self.cells = {
             0:[Position(0,0), Position(0,1), Position(1,1), Position(1,2)],
-            1:[Position(0,2), Position(1,1), Position(1,2), Position(2,0)],
+            1:[Position(0,2), Position(1,1), Position(1,2), Position(2,1)],
             2:[Position(1,0), Position(1,1), Position(2,1), Position(2,2)],
             3:[Position(0,1), Position(1,0), Position(1,1), Position(2,0)]
         }
